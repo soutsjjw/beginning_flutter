94822d49702be8a2464961d93ae042c8cf5d4034
souts jjw <souts.jjw@gmail.com>
2021-10-16 15:54:00 +0800

    ch4_starter_exercise

---
+++ Chapter 4/ch4_starter_exercise/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:ch4_starter_exercise/pages/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Starter Template',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
